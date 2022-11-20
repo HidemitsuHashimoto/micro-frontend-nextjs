e6483fdf0abea4c7c36c62ef54d4ff18f7a041bc
Hidemitsu <hidemitsu.hashimoto@gmail.com>
2022-11-20 13:14:54 -0300

    feat: add nav in shop app

--- shop-app/components/Header.tsx
+++ shop-app/components/Header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <div
@@ -8,12 +10,23 @@ const Header = () => {
         color: 'white',
         textAlign: 'center',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'left',
         alignItems: 'center',
+        gap: '1rem',
         fontSize: '24px',
         paddingLeft: '15px'
       }}>
       Shop Header
+      <nav>
+        <div style={{
+          borderRadius: '8px',
+          backgroundColor: '#D1D1D1',
+          padding: '1rem 2rem',
+        }}>
+        <Link href='/about'>About</Link>
+        </div>
+      </nav>
     </div>
   );
 };
---
+++ shop-app/pages/about/index.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <section>
+      <h1>About Page</h1>
+      <p>This is the shop about page.</p>
+    </section>
+  )
+}
+
+export default About
